a89576c28e86e3905ab1998ce7371d0599fb1ddd
Raz Ben Simon <razbensimon2@gmail.com>
2025-01-08 16:29:45 +0200

    Update main.tf

--- misc/module-registry-test/main.tf
+++ misc/module-registry-test/main.tf
@@ -1,6 +1,6 @@
 module "raz" {
   source = "api-pr17883.dev.env0.com/f3850937-c125-4978-8cab-a25602348807/raz/raz2"
-  version = "0.0.11"
+  version = "0.1.0"
 }
 
 resource "null_resource" "null" {}
